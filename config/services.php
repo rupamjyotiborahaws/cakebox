18aec1104f52785f4b5ae8fc0603940a0e961cab
rupamjyotiborahworxogo <rupamjyoti.borah@worxogo.com>
2025-04-04 10:42:05 +0530

    Changes in config file

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'twilio' => [
         'sid' => env('TWILIO_SID'),
         'token' => env('TWILIO_AUTH_TOKEN'),
-        'from' => env('TWILIO_PHONE_NUMBER'),
+        'from' => env('TWILIO_FROM'),
     ],
 
 ];
